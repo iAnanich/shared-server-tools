d58570a0c7d9a60c94d029fb682bf555a8b2414b
Illia Ananich <illia.ananich@gmail.com>
2020-07-04 13:27:07 +0300

    explain `example.register.sh`

--- gitlab-ci-runner/example.register.sh
+++ gitlab-ci-runner/example.register.sh
@@ -1,14 +1,20 @@
 
 # https://docs.gitlab.com/runner/register/index.html#one-line-registration-command
 
-docker run --rm -v /srv/gitlab-runner/config:/etc/gitlab-runner gitlab/gitlab-runner register \
+# Using Docker "gitlab-runner-config" volume.
+# Common tags include: test, build, stag, prod
+# Replace `RUNNER_REGISTRATION_TOKEN` with token from repo/group settings > CI > runners section
+
+docker run --rm -v gitlab-runner-config:/etc/gitlab-runner \
+  gitlab/gitlab-runner register \
   --non-interactive \
   --executor "docker" \
-  --docker-image alpine:latest \
-  --url "https://gitlab.com/" \
-  --registration-token "PROJECT_REGISTRATION_TOKEN" \
-  --description "docker-runner" \
-  --tag-list "docker" \
-  --run-untagged="true" \
+  --docker-image docker:latest \
+  --docker-volumes /var/run/docker.sock:/var/run/docker.sock \
+  --description "Docker runner" \
   --locked="false" \
-  --access-level="not_protected"
+  --access-level="not_protected" \
+  --run-untagged="true" \
+  --tag-list "docker" \
+  --url "https://gitlab.com/" \
+  --registration-token "RUNNER_REGISTRATION_TOKEN"
