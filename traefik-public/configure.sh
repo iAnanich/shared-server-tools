106c675d5ede1936f0a29e59bf3057b03ebbc2cc
Illia Ananich <illia.ananich@gmail.com>
2020-07-24 11:08:43 +0300

    traefik-public: allow setting key type (default - ec384) and ca server (default - LE staging)

--- traefik-public/configure.sh
+++ traefik-public/configure.sh
@@ -8,6 +8,10 @@ echo
 read -r -p "Overwrite? [Y/n] " input
 case $input in
     [yY][eE][sS]|[yY])
+  read -p "TLS key type ['EC256', 'EC384', 'RSA2048', 'RSA4096', 'RSA8192'] (leave blank for default EC384): " acme_keytype
+  acme_keytype=${acme_keytype:-"EC384"}
+  read -p "CA Server (leave blank for default - LE staging): " acme_caserver
+  acme_caserver=${acme_caserver:-"https://acme-staging-v02.api.letsencrypt.org/directory"}
   read -p "E-mail address for ACME challenge: " acme_email
   read -p "Traefik dashboard domain (something like traefik.example.com): " traefik_domain
   read -p "Traefik admin username: " traefik_user
@@ -16,6 +20,8 @@ case $input in
   case $input in
       [yY][eE][sS]|[yY])
     echo "" > .env
+    echo "ACME_KEYTYPE=$acme_keytype" >> .env
+    echo "ACME_CASERVER=$acme_caserver" >> .env
     echo "ACME_EMAIL=$acme_email" >> .env
     echo "TRAEFIK_DOMAIN=$traefik_domain" >> .env
     echo "TRAEFIK_USER=$traefik_user" >> .env
